03f7b30e58b559dfdb7bc0f26f4845cbb4dc51cf
Ralf Kaestner <ralf.kaestner@gmail.com>
2015-03-12 09:04:01 +0100

    Initial commit with Prolog OWL parser

---
+++ prolog/init.pl
@@ -0,0 +1,7 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% dependencies
+
+:- register_ros_package(knowrob_common).
+:- register_ros_package(knowrob_semantic_map_tools).
+
+:- use_module(library('owl_string_parser')).
---
+++ prolog/owl_string_parser.pl
@@ -0,0 +1,46 @@
+:- module(owl_string_parser, [
+      owl_parse_string/1,              % +String
+      owl_parse_string_from_file/1     % +File
+   ]).
+
+:- use_module(library('semweb/rdf_db.pl')).
+:- use_module(library('semweb/rdf_edit.pl')).
+:- use_module(library('semweb/rdfs.pl')).
+:- use_module(library('memfile.pl')).
+
+%% owl_parse_string(+String)
+%
+% Parse an OWL string and load it into the local RDF database.
+% 
+% Resolves owl:imports and supports both the common URL formats
+% (file paths, file:// or http://) and the package:// URLs used
+% in ROS to reference files with respect to the surrounding ROS
+% package.
+%
+% @param String OWL string
+% 
+owl_parse_string(String) :-
+  new_memory_file(Handle),
+  open_memory_file(Handle, write, OutStream),
+  write(OutStream, String),
+  close(OutStream),
+  open_memory_file(Handle, read, InStream),
+  rdf_db:rdf_load(InStream, [format(xml)]),
+  close(InStream),
+  free_memory_file(Handle),
+  true.
+
+%% owl_parse_string_from_file(+File)
+%
+% Parse an OWL string read from a file and load it into the local RDF
+% database.
+%
+% @param File Path to OWL file
+%
+owl_parse_string_from_file(File) :-
+  open(File, read, Stream),
+  read_stream_to_codes(Stream, Codes),
+  string_codes(String, Codes),
+  close(Stream),
+  owl_parse_string(String),
+  true.
